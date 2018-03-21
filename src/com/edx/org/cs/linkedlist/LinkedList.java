6b6eb0c83d856b74052449a3ba9c9a9b781b044b
Wilton Cardona <faltdor@gmail.com>
2018-03-21 13:10:15 -0500

    Linkded list operations

--- src/com/edx/org/cs/linkedlist/LinkedList.java
+++ src/com/edx/org/cs/linkedlist/LinkedList.java
@@ -55,4 +55,76 @@ public class LinkedList {
             }
         }
     }
+
+    public boolean contains(int value){
+        Node current = head;
+        while (current != null){
+            if(current.value == value){
+                return  true;
+            }
+            current = current.next;
+        }
+        return  false;
+    }
+
+    public int getByIndex(int index){
+        if(index < 0){
+            throw  new IndexOutOfBoundsException();
+        }else {
+            Node current = head;
+            for (int i = 0; i < index; i++) {
+                if(current == null || current.next == null){
+                    throw  new IndexOutOfBoundsException();
+                }
+                current = current.next;
+            }
+            return  current.value;
+        }
+    }
+
+    public  void removeFromFront(){
+        if (head != null){
+            head = head.next;
+        }
+
+        if (head == null){
+            tail = nul;
+        }
+    }
+
+    public  void removeFromBack(){
+        if (head == null){
+            return;
+        }else if (head.equals(tail)){
+            head = null;
+            tail = null;
+        }else {
+            Node current = head;
+            while (current.next != tail) {
+                current = current.next;
+            }
+            tail = current;
+            current.next = null;
+        }
+    }
+
+    public  void removeAtIndex(int index){
+        if(index < 0){
+            throw  new IndexOutOfBoundsException();
+        }else if (index == 0){
+            removeFromFront();
+        }else {
+            Node current = head;
+            for (int i = 0; i < index; i++) {
+                if(current == null){
+                    throw  new IndexOutOfBoundsException();
+                }
+                current = current.next;
+            }
+            current.next = current.next.next;
+            if(current.next == null){
+                tail = current;
+            }
+        }
+    }
 }
